9609a1e3d0bd5717c2149acea1af2e148e4081a3
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-20 22:42:16 +0900

    メインのロジックを実装
    - ローカルで動くことは確認済
    - 愚直に書いているので、もう少し綺麗にしたい
    - RSpecのテストをちゃんと書く

--- lib/wakaba.rb
+++ lib/wakaba.rb
@@ -4,5 +4,73 @@ require_relative "wakaba/version"
 
 module Wakaba
   class Error < StandardError; end
-  # Your code goes here...
+
+  module ExceptionJp
+    def message
+      case self
+      when NameError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐もしかして:
+          - #{name}がタイポかも？
+          - #{name}を呼び出すためのrequireを忘れているかも？
+        "
+
+      when NoMethodError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐もしかして:
+          - #{name}という名前のメソッドは存在しないかも？
+          - #{name}を呼び出したけど、レシーバがnilかも？
+        "
+
+      when TypeError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐期待していない型がメソッドの引数に渡されているかも？
+        "
+      when ArgumentError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐もしかして:
+          - メソッドの引数の数が違うかも？
+          - メソッドに渡した値が期待した値ではないかも？
+        "
+      when ZeroDivisionError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐0で除算した場合にこのエラーが発生するよ
+        "
+      when SystemStackerror
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐無限ループの場合にこのエラーが発生するよ
+        "
+      when Loaderror
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐もしかして:
+          - requireしたいファイルのパスかライブラリ名が間違っているかも？
+          - requireしたgemが実行環境にないかも？
+        "
+      when SyntaxError
+        "
+        \e[31m#{to_s}\e[0m
+
+        🧐end,カンマ,括弧の数を確認してみよう
+        "
+      else
+        super
+      end
+    end
+  end
+
+  Exception.prepend(ExceptionJp)
 end
