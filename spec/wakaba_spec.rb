cb31b4b325cbc4e972d3083c24a680dfcf7bff64
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-20 22:23:59 +0900

    initial commit

---
+++ spec/wakaba_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Wakaba do
+  it "has a version number" do
+    expect(Wakaba::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
