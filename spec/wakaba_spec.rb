ace76f4f2add182d4117ebf2d19f6e2ac7ff4098
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-23 15:19:47 +0900

    RSpecのテストを準備した

--- spec/wakaba_spec.rb
+++ spec/wakaba_spec.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe Wakaba do
-  it 'has a version number' do
-    expect(Wakaba::VERSION).not_to be nil
+  it 'Expect a NameError message' do
+    error = NameError.new
+    expect(error.output_message(error)).to include('という名前のメソッドは存在しないかも')
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'Expect a NoMethodError message' do
+    error = NoMethodError.new
+    expect(error.output_message(error)).to include('がタイポかも？')
+  end
+
+  it 'Expect a TypeError message' do
+    error = TypeError.new
+    expect(error.output_message(error)).to include('期待していない型')
+  end
+
+  it 'Expect a ArgumentError message' do
+    error = ArgumentError.new
+    expect(error.output_message(error)).to include('メソッドの引数の数が違う')
+  end
+
+  it 'Expect a ZeroDivisionError message' do
+    error = ZeroDivisionError.new
+    expect(error.output_message(error)).to include('0で除算した場合')
   end
 end
